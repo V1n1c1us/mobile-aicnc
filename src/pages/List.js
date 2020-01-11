f2633b22d6cd4314004ac1cb65c096efb50eaa4c
V1n1c1us <v.f.diehl@gmail.com>
2020-01-11 00:37:12 -0300

    Was created login

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, AsyncStorage } from 'react-native'
+
+import api from '../services/api';
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View>
-        <Text>Olá List</Text>
-    </View>
+
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            const techsArray = storageTechs.slice(',').localeCompare(tech => tech.trim());
+            setTechs(techsArray);
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 50,
+    }
+});
