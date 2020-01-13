13bb849a2812ba4f429332a421fd8481cb39b362
V1n1c1us <v.f.diehl@gmail.com>
2020-01-12 23:53:24 -0300

    Spot list has been created and allows spot booking

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Text, StyleSheet, Image, AsyncStorage } from 'react-native'
+import { SafeAreaView, Text, StyleSheet, Image, AsyncStorage, ScrollView } from 'react-native'
+
 
 import api from '../services/api';
 import logo from '../assets/logo.png';
+import SpotList from '../components/SpotList';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function List() {
+export default function List({ navigation }) {
 
     const [techs, setTechs] = useState([]);
 
     useEffect(() => {
-        AsyncStorage.getItem('techs').then(storageTechs => {
-            const techsArray = storageTechs.slice(',').localeCompare(tech => tech.trim());
-            setTechs(techsArray);
-        })
+        AsyncStorage.getItem('techs')
+            .then((storagedTechs) => {
+                const techsArray = storagedTechs.split(',').map(tech => tech.trim())
+
+                setTechs(techsArray)
+            })
     }, [])
 
+    function backLogin() {
+        AsyncStorage.removeItem('user');
+        navigation.navigate('Login');
+
+    }
     return (
         <SafeAreaView style={styles.container}>
             <Image style={styles.logo} source={logo} />
+
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+            <TouchableOpacity style={styles.button} onPress={backLogin}>
+                <Text style={styles.buttonText}>Sair</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
@@ -31,5 +48,18 @@ const styles = StyleSheet.create({
         resizeMode: "contain",
         alignSelf: "center",
         marginTop: 50,
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        margin: 30,
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16
     }
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api-aircnc.herokuapp.com',
+    baseURL: 'http://api-aircnc.herokuapp.com',
 })
 
 export default api;
